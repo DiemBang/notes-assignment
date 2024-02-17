0a648557257ce8b14073097657158146c57c0beb
Diem Bang <diembang09@gmail.com>
2024-02-17 23:36:24 +0100

    Create endpoints: get all docs and create new doc

--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -1,9 +1,46 @@
-var express = require('express');
+var express = require("express");
+const connection = require("../lib/conn");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET all docs */
+router.get("/", function (req, res) {
+  connection.connect((err) => {
+    if (err) console.log("err", err);
+
+    let query = "SELECT * FROM documents";
+
+    connection.query(query, (err, data) => {
+      if (err) console.log("err", err);
+      console.log("documents", data);
+      res.json(data);
+    });
+  });
 });
+
+
+/* CREATE new doc */
+
+router.post("/new", (req, res) => {
+
+    let user = req.body.user;
+    let name = req.body.name;
+    let content = req.body.content;
+
+    connection.connect((err) => {
+      if (err) console.log("err", err);
+
+      let query = "INSERT into documents (user, name, content) VALUES (?, ?, ?)";
+      let values = [user, name, content];
+
+      connection.query(query, values, (err, data) => {
+        if (err) console.log("err", err);
+
+        console.log("new document", data);
+        res.json({message: "new doc created"});
+      });
+    });
+});
+
+
 
 module.exports = router;
