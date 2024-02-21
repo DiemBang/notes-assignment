838ea6148e470cf1d9de2b40bec1c311b7e65c93
Diem Bang <diembang09@gmail.com>
2024-02-21 21:29:18 +0100

    Add create new user feature in frontend

--- frontend/document/printLogin.js
+++ frontend/document/printLogin.js
@@ -7,79 +7,146 @@ let docList = document.getElementById("docList");
 let myDocs = document.getElementById("myDocs");
 let newDocTextArea = document.getElementById("newDocTextArea");
 let viewDoc = document.getElementById("viewDoc");
+let signUpForm = document.getElementById("signUpForm");
+let signUpLink = document.getElementById("signUpLink");
 
 function printLoginForm() {
-    loginForm.innerHTML = "";
-    let inputUsername = document.createElement("input");
-    inputUsername.placeholder = "Username";
-    let inputPassword = document.createElement("input");
-    inputPassword.placeholder = "Password";
-    inputPassword.type = "password";
-    let loginBtn = document.createElement("button");
-    loginBtn.id = "loginBtn";
-    loginBtn.innerText = "Log in";
-    let signUp = document.createElement("div");
-    signUp.innerText = "Don't have an account yet?";
-    let signUpLink = document.createElement("div");
-    signUpLink.innerText = "Sign up now.";
-  
-    loginBtn.addEventListener("click", () => {
-      let sendUser = { username: inputUsername.value, password: inputPassword.value };
-  
-      fetch("http://localhost:3000/users/login", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(sendUser)
-      })
-      .then(res => res.json()) 
-      .then(data => {
-          console.log("post user", data);
-  
-          if (data.id) {
-              localStorage.setItem("user", data.id);
-              handleUserLoggedIn();
-          } else {
-              alert("Fel inlogg");
-          }
-      })
-    });
+  loginForm.innerHTML = "";
+  let inputUsername = document.createElement("input");
+  inputUsername.placeholder = "Username";
+  let inputPassword = document.createElement("input");
+  inputPassword.placeholder = "Password";
+  inputPassword.type = "password";
+  let loginBtn = document.createElement("button");
+  loginBtn.id = "loginBtn";
+  loginBtn.innerText = "Log in";
+  let signUpText = document.createElement("div");
+  signUpText.innerText = "Don't have an account yet?";
+  let signUpLink = document.createElement("div");
+  signUpLink.innerText = "Sign up now.";
 
-    //add event listener to sign up now link
+  loginBtn.addEventListener("click", () => {
+    let sendUser = {
+      username: inputUsername.value,
+      password: inputPassword.value,
+    };
+
+    fetch("http://localhost:3000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sendUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("post user", data);
+
+        if (data.id) {
+          localStorage.setItem("user", data.id);
+          handleUserLoggedIn();
+        } else {
+          alert("Fel inlogg");
+        }
+      });
+  });
+
+  signUpLink.addEventListener("click", () => {
+    printSignUpFormAndCreateNewUserBtn();
+  });
+
+  loginForm.append(
+    inputUsername,
+    inputPassword,
+    loginBtn,
+    signUpText,
+    signUpLink
+  );
+}
+
+function printSignUpFormAndCreateNewUserBtn() {
+    console.log("printSignUpFormAndCreateNewUserBtn");
+  signUpForm.innerHTML = "";
+
+  let inputName = document.createElement("input");
+  inputName.placeholder = "Name";
+
+  let inputUsername = document.createElement("input");
+  inputUsername.placeholder = "Username";
+
+  let inputPassword = document.createElement("input");
+  inputPassword.placeholder = "Password";
+  inputPassword.type = "password";
+
+  let createNewUserBtn = document.createElement("button");
+  createNewUserBtn.id = "createNewUserBtn";
+  createNewUserBtn.innerText = "Create new user";
+
+  createNewUserBtn.addEventListener("click", () => {
+    console.log("click click");
+    let newUser = {
+        name: inputName.value,
+        username: inputUsername.value,
+        password: inputPassword.value
+      };
+    createNewUser(newUser);
+  });
+
+  signUpForm.append(inputName, inputUsername, inputPassword, createNewUserBtn);
+};
+
+function createNewUser(newUser) {
   
-    loginForm.append(inputUsername, inputPassword, loginBtn, signUp, signUpLink);
+  console.log("create", newUser);
+
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("response from server", data);
+      localStorage.setItem("user", data.id);
+      handleUserLoggedIn();
+    });
 };
 
 function handleUserLoggedIn() {
-    printLogoutBtn();
-    printDocs(); // temporary, swap for print docs for specific user
-    showCreateNewDocBtn();    
-};
+  printLogoutBtn();
+  printDocs();
+  showCreateNewDocBtn();
+  signUpForm.innerHTML = "";
+}
 
 function handleUserLoggedOut() {
-    newDoc.innerHTML = "";
-    docList.innerHTML = "";
-    printLoginForm();
-    myDocs.innerHTML = "";
-    newDocTextArea.innerHTML = "";
-    viewDoc.innerHTML = "";
+  newDoc.innerHTML = "";
+  docList.innerHTML = "";
+  printLoginForm();
+  myDocs.innerHTML = "";
+  newDocTextArea.innerHTML = "";
+  viewDoc.innerHTML = "";
 };
 
 function printLogoutBtn() {
-    loginForm.innerHTML = "";
+  loginForm.innerHTML = "";
 
-    let logoutBtn = document.createElement("button");
-    logoutBtn.innerText = "Log out";
+  let logoutBtn = document.createElement("button");
+  logoutBtn.innerText = "Log out";
 
-    logoutBtn.addEventListener("click", () => {
-        localStorage.removeItem("user");
-        handleUserLoggedOut();
-    })
+  logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    handleUserLoggedOut();
+  });
 
-    loginForm.appendChild(logoutBtn);
-
+  loginForm.appendChild(logoutBtn);
 };
 
-
-export { printLoginForm, printLogoutBtn, handleUserLoggedIn, handleUserLoggedOut };
+export {
+  printLoginForm,
+  printLogoutBtn,
+  handleUserLoggedIn,
+  handleUserLoggedOut,
+};
